f27c6e4dcdd461229dd20ce28dbcb4a644f797d4
G.Burak <webyonet@hotmail.com>
2014-12-02 15:34:18 +0200

    Update angular-or-filter.js

--- src/angular-or-filter.js
+++ src/angular-or-filter.js
@@ -1,7 +1,7 @@
-﻿/*
+/*
  * @name: Angularjs Or Filter
  * @type: Filter
- * @version: v1.0.0
+ * @version: v1.3.2
  * @author: Burak Demirezen
  * @License: Licensed under the MIT license.
  */
@@ -17,42 +17,54 @@
                     return obj;
                 }
             },
-                equals = function (obj1, obj2, type) {
-                    if (type) return obj1 === obj2;
-                    else {
-                        if (angular.isString(obj1) && angular.isString(obj2))
-                            return obj1.toLocaleLowerCase() == obj2.toLocaleLowerCase();
-                        else
-                            return obj1 == obj2;
-                    }
-                };
+            equals = function (obj1, obj2, type) {
+                if (type) return obj1 === obj2;
+                else {
+                    if (angular.isString(obj1) && angular.isString(obj2))
+                        return obj1.toLocaleLowerCase() == obj2.toLocaleLowerCase();
+                    else
+                        return obj1 == obj2;
+                }
+            },
+            isEmpty = function (obj) {
+                if (obj == null) return true;
+                for (var name in obj) {
+                    return false;
+                }
+                return true;
+            }
 
             return function (array, expression, comparator) {
                 var keys = keyParser(expression),
                     returnList = [],
                     keyIsArray = angular.isArray(keys);
                 comparator = comparator || false;
-               
-                for (var ary in array) {
-                    if (keyIsArray && !angular.isArray(expression)) {
-                        for (var key in keys) {
-                            if (equals(array[ary][keys[key]], expression[keys[key]], comparator)) {
-                                returnList.push(array[ary]);
-                                break;
+
+                if (isEmpty(expression)) {
+                    console.log(expression);
+                    returnList = array;
+                } else {
+                    for (var ary in array) {
+                        if (keyIsArray && !angular.isArray(expression)) {
+                            for (var key in keys) {
+                                if (equals(array[ary][keys[key]], expression[keys[key]], comparator)) {
+                                    returnList.push(array[ary]);
+                                    break;
+                                }
                             }
-                        }
-                    } else if (keyIsArray) {
-                        for (var key in keys) {
-                            if (equals(keys[key], array[ary], comparator)) {
+                        } else if (keyIsArray) {
+                            for (var key in keys) {
+                                if (equals(keys[key], array[ary], comparator)) {
+                                    returnList.push(array[ary]);
+                                }
+                            }
+                        } else {
+                            if (equals(array[ary], expression, comparator)) {
                                 returnList.push(array[ary]);
                             }
-                        }
-                    } else {
-                        if (equals(array[ary], expression, comparator)) {
-                            returnList.push(array[ary]);
                         }
                     }
-                };
+                }
 
                 return returnList;
             }
